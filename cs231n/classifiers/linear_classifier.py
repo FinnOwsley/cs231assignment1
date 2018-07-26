11a7f50cc15aeb38d18f5765a90b8d53fc063be9
FinnOwsley <Finn.Owsley@echonous.com>
2018-07-26 09:04:26 -0700

    Updated

--- cs231n/classifiers/linear_classifier.py
+++ cs231n/classifiers/linear_classifier.py
@@ -51,7 +51,9 @@ class LinearClassifier(object):
       # Hint: Use np.random.choice to generate indices. Sampling with         #
       # replacement is faster than sampling without replacement.              #
       #########################################################################
-      pass
+      batchInds = np.random.choice(X.shape[0], batch_size)
+      x_batch = X[batchInds].transpose()
+      y_batch = y[batchInds]
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
@@ -65,7 +67,7 @@ class LinearClassifier(object):
       # TODO:                                                                 #
       # Update the weights using the gradient and the learning rate.          #
       #########################################################################
-      pass
+      self.W -= learning_rate * grad
       #########################################################################
       #                       END OF YOUR CODE                                #
       #########################################################################
--- cs231n/classifiers/linear_svm.py
+++ cs231n/classifiers/linear_svm.py
@@ -59,7 +59,6 @@ def svm_loss_naive(W, X, y, reg):
 
   return loss, dW
 
-
 def svm_loss_vectorized(W, X, y, reg):
   """
   Structured SVM loss function, vectorized implementation.
@@ -73,6 +72,8 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
+  print(type(X))
+  print(type(W))
   scores = np.dot(X, W)
   correct_class_scores = scores[np.arange(scores.shape[0]), y][:, np.newaxis]
   loss = np.sum(np.maximum(0, scores-correct_class_scores+1)) / X.shape[0] + reg * np.sum(W * W)
@@ -90,13 +91,14 @@ def svm_loss_vectorized(W, X, y, reg):
   # loss.                                                                     #
   #############################################################################
   passed = np.maximum(0, scores - correct_class_scores + 1)
+  passed[np.arange(X.shape[0]), y] = 0
   passed[passed!=0] = 1
+  passedSum = np.sum(passed, axis = 1)
+  passed[np.arange(X.shape[0]), y] = -passedSum
   dW += np.dot(X.transpose(), passed)
-  passed = np.sum(passed, axis = 1)
-  print(passed.shape)
-  #a = np.sum(np.dot(X.transpose(), passed), axis = 1) 
-  dW[:, y] -= X.transpose()*passed
   dW /= X.shape[0]
+  dW += reg * W
+    
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
